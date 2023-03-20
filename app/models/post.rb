3b09190ed481e0875ef61822c92233d3cc27f0ef
Glen <glen.kimani@student.moringaschool.com>
2023-03-20 14:02:50 +0300

    Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validate :fiction_or_non_fiction
+    validates :content, length: {minimum:100}
+
+    def fiction_or_non_fiction
+        unless ["Non-Fiction", "Fiction"].include?(self.category)
+            errors.add :category, "is not included in the list"
+        end
+    end
 end
